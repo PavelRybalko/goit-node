75be92e4a3a7c2b56fd868232ff910b7cc8d476e
PavelRybalko <freedom277f@gmail.com>
2021-02-14 19:02:35 +0200

    working project

---
+++ index.js
@@ -0,0 +1,47 @@
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContacts,
+} from "./contacts.js";
+
+import commander from "commander";
+const { Command } = commander;
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .requiredOption("-a, --action <type>", "Input folder")
+  .option("id, --id [type]", "Id contact")
+  .option("n, --name [type]", "Name contact")
+  .option("e, --email [type]", "Email contact")
+  .option("p, --phone [type]", "Phone contact")
+  .option("d, --delete", "Delete contact");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact({ name, email, phone });
+      break;
+
+    case "remove":
+      removeContacts(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
